55ecd7f5c24e7c343d7cf78b8d15f683b090b728
Kristie Nguyen <kristietamato@gmail.com>
2016-07-05 16:47:13 -0700

    Modify data and incorporate data into app using a service

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,7 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MyController', function MyController($scope) {
-    
-});
+myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+  $http.get('resources/data/vegetables.json').success(function(data) {
+    $scope.vegetables = data;
+  });
+}]);
