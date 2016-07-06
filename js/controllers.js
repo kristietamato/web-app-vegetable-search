342821a7febcbc973baf17cd697df150979f0932
Kristie Nguyen <kristietamato@gmail.com>
2016-07-05 17:51:28 -0700

    Add ascending and descending order using data binding and filters

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,6 @@ var myApp = angular.module('myApp', []);
 myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
   $http.get('resources/data/vegetables.json').success(function(data) {
     $scope.vegetables = data;
+    $scope.vegetableOrder = 'name';
   });
 }]);
