2ecccf57dbb7d61cd328540eb1114a1037fa69fe
Kristie Nguyen <kristietamato@gmail.com>
2016-07-07 13:56:24 -0700

    Set up angular module to redirect to list.html

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
-var vegetableController = angular.module('vegetableController', []);
+var vegetableControllers = angular.module('vegetableControllers', []);
 
-vegetableController.controller('ListController', ['$scope', '$http', function($scope, $http) {
+vegetableControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
   $http.get('resources/data/vegetables.json').success(function(data) {
     $scope.vegetables = data;
     $scope.vegetableOrder = 'name';
